74661a747766dc199eeada1c75ed8de11ef4da5f
p1xel <lcs2020024@iiitl.ac.in>
2021-09-07 16:26:08 +0530

    Added exception handling for ques2

--- assignment-5/ques2/Main.java
+++ assignment-5/ques2/Main.java
@@ -7,12 +7,21 @@ class Main{
 		ArrayList<Integer> nums = new ArrayList<Integer>();
 		System.out.printf("\n====================================\nENTER FIVE NUMBERS : ");
 		for(int i=0; i<5; i++){
-			nums.add(sc.nextInt());
+			try{
+				nums.add(sc.nextInt());
+			}catch(InputMismatchException e){
+				System.out.println("You entered an invalid number!");
+			}
 		}
 
 		FindSum n1 = new FindSum();
 		FindMin n2 = new FindMin();
-		int ans = n1.ReturnSum(nums)/n2.ReturnMin(nums);
+		try{
+			int ans = n1.ReturnSum(nums)/n2.ReturnMin(nums);
+		}catch(ArithmeticException e){
+			System.out.println("0 cannot be one of the numbers!");
+			System.out.exit(0);
+		}
 		System.out.printf("\n===============ANSWER===============\n%d\n====================================\n", ans);
 	}
 }
